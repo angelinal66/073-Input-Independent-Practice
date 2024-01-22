aad903c7edd57f29fc38891beda125015b8175ea
angelinal66 <144931300+angelinal66@users.noreply.github.com>
2024-01-22 12:27:24 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".poy").click(function() {
+    var ang=$(".v").val();
+console.log(ang);
+$(".wu").text(ang);    
+    $(".dinner").show();
+    alert("Thank You For Voting");
+   
+   
+});
+$(".nv").click(function() {
+    var lin=$(".SN").val();
+console.log(lin);
+$(".reslt").text(lin);    
+    $(".sn").show();
+    alert("Thank You For Voting");
+});
